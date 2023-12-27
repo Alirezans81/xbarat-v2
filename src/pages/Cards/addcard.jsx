a0df55bf1dcf7f6e9528134b3c973cb92892b7cc
Sina <sinabook20031381@gmail.com>
2023-12-27 18:02:07 +0330

    Add card complete with question though

--- src/pages/Cards/addcard.jsx
+++ src/pages/Cards/addcard.jsx
@@ -25,7 +25,6 @@ const Addcard = ({ addCard, setAddCard, show }) => {
   const [cardNumber, setCardNumber] = useState("");
   const [shabaNumber, setShabaNumber] = useState("");
   const [currency, setCurrency] = useState("");
-
   const [params, setParams] = useState({
     wallet_asset: "",
     wallet_tank_type: "",
@@ -36,9 +35,10 @@ const Addcard = ({ addCard, setAddCard, show }) => {
     bank_info: "",
   });
   useEffect(()=>{
-    if(show){
+    if(show.length!==0){
       setAsset(show)
     }
+  
   })
   console.log(asset)
   const { createWalletTank, isLoading: createWalletTankIsLoading } =
@@ -48,17 +48,17 @@ const Addcard = ({ addCard, setAddCard, show }) => {
   }, [createWalletTankIsLoading]);
 
   useEffect(() => {
-    if (title && (cardNumber || shabaNumber)) {
+    if (title && type && asset &&(cardNumber || shabaNumber)) {
       setParams({
         wallet_asset: LinkedWalletAsset[0].url,
-        wallet_tank_type: cardNumber
+        wallet_tank_type: type==="Card"
           ? "https://smicln.ir/api/wallet/tank/type/card-number/"
           : "https://smicln.ir/api/wallet/tank/type/shaba-number/",
         title: title,
         balance: 0,
         locked: 0,
         pending: 0,
-        bank_info: cardNumber?cardNumber:shabaNumber,
+        bank_info: type==="Card"?cardNumber:shabaNumber,
       });
     }
   }, [title, cardNumber, shabaNumber]);
@@ -85,7 +85,7 @@ const Addcard = ({ addCard, setAddCard, show }) => {
   let AvailableNewAssets = listCurrency.filter(
     (data) => listAsset.includes(data) === false
   );
-
+    
   return (
     <>
       <div
@@ -98,105 +98,105 @@ const Addcard = ({ addCard, setAddCard, show }) => {
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
 
         <div
-          style={{ height: "36%" }}
-          className={`bg-${theme} flex flex-col w-1/4 h-fit px-5 py-5 rounded-lg z-50`}
+          style={{ height: "fit-content" }}
+          className={`bg-${theme} flex flex-col w-1/4 px-5 py-5 rounded-lg z-50`}
         >
           <button
             className="flex justify-end h-fit"
             onClick={discard}
-            style={{ gridRow: 1, gridColumn: 1 }}
           >
             <img className="w-6" src={cross} alt="" />
           </button>
           <form onSubmit={handleAddCards} className="w-full h-full">
-            <div className="flex flex-col">
-              <span
-                className={`text-${oppositeTheme} text-xl font-${font}-bold mt-3`}
-              >
-                {lang["add_cards_title"]}
-              </span>
-              <div className=" w-full">
-                <div className="w-full flex mt-0 px-2">
-                  <input
-                    onChange={(e) => setTitle(e.target.value)}
-                    required
-                    className={`flex-1 hide-input-arrows text-center-important font-${font}-regular text-${oppositeTheme} border border-gray bg-${theme} px-3 outline-1 h-9 outline-white rounded-lg w-full pt-2 pb-1 mb-3`}
-                    placeholder={lang["add_cards_title"]}
-                  />
+                <div className="flex flex-col">
+                    <span
+                      className={`text-${oppositeTheme} text-xl font-${font}-bold mt-3`}
+                    >
+                      {lang["add_cards_title"]}
+                    </span>
+                    <div className=" w-full">
+                      <div className="w-full flex mt-0 px-2">
+                        <input
+                          onChange={(e) => setTitle(e.target.value)}
+                          required
+                          className={`flex-1 hide-input-arrows text-center-important font-${font}-regular text-${oppositeTheme} border border-gray bg-${theme} px-3 outline-1 h-9 outline-white rounded-lg w-full pt-2 pb-1 mb-3`}
+                          placeholder={lang["add_cards_title"]}
+                        />
+                      </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-            <div className={asset.length!==0?"hidden":"flex justify-center w-full px-2"}>
-            <CustomDropdown
-                className={"bg-transparent"}
-                label={currency.length === 0 ? lang["currency"] : currency}
-              >
-                {AvailableNewAssets.map((data) => (
-                  <CustomItem
-                    onClick={() => setAsset(data)}
-                    className={"bg-transparent"}
-                  >
-                    {data}
-                  </CustomItem>
-                ))}
-              </CustomDropdown>
-            </div>
-            <div className={type.length!==0?"hidden":"flex justify-center w-full px-2"}>
-            <CustomDropdown
-                className={"bg-transparent"}
-                label={type.length===0?"Please Select One of the following":type}
-              >
-                <CustomItem onClick={()=>setType("Card")}>
-                      Card Number                  
-                </CustomItem>
-                <CustomItem onClick={()=>setType("Shaba")}>
-                      Shaba Number
-                </CustomItem>
-              </CustomDropdown>
-            </div>
 
-            <div className={type?"block":"hidden"}>
-                <div className={type==="Card"?"flex flex-col":"hidden"}>
-                  <span
-                    className={`text-${oppositeTheme} text-xl font-${font}-bold mt-1`}
-                  >
-                    {lang["cards_card_number"]}
-                  </span>
-                  <div className=" w-full">
-                    <div className="w-full flex mt-0 px-2">
-                      <input
-                        onChange={(e) => setCardNumber(e.target.value)}
-                        className={`flex-1 hide-input-arrows text-center-important font-${font}-regular text-${oppositeTheme} border border-gray bg-${theme} px-3 outline-1 h-9 outline-white rounded-lg w-full pt-2 pb-1 mb-3`}
-                        placeholder={lang["cards_card_number_placeholder"]}
-                      />
+                
+                <div className={asset.length!==0?"hidden":"flex justify-center w-full px-2 py-5"}>
+                  <CustomDropdown
+                      className={"bg-transparent"}
+                      label={currency.length === 0 ? lang["currency"] : currency}
+                    >
+                      {AvailableNewAssets.map((data) => (
+                        <CustomItem
+                          onClick={() => setAsset(data)}
+                          className={"bg-transparent"}
+                        >
+                          {data}
+                        </CustomItem>
+                      ))}
+                    </CustomDropdown>
+                </div>
+                <div className={type.length!==0?"hidden":"flex justify-center w-full px-2"}>
+                      <CustomDropdown
+                        className={"bg-transparent"}
+                        label={type.length===0?"Card/Shaba":type}
+                      >
+                          <CustomItem onClick={()=>setType("Card")}>
+                                Card Number                  
+                          </CustomItem>
+                          <CustomItem onClick={()=>setType("Shaba")}>
+                                Shaba Number
+                          </CustomItem>
+                      </CustomDropdown>
+                </div>
+
+                <div className={type?"block":"hidden"}>
+                    <div className={type==="Card"?"flex flex-col":"hidden"}>
+                      <span
+                        className={`text-${oppositeTheme} text-xl font-${font}-bold mt-1`}
+                      >
+                        {lang["cards_card_number"]}
+                      </span>
+                      <div className=" w-full">
+                        <div className="w-full flex mt-0 px-2">
+                          <input
+                            onChange={(e) => setCardNumber(e.target.value)}
+                            className={`flex-1 hide-input-arrows text-center-important font-${font}-regular text-${oppositeTheme} border border-gray bg-${theme} px-3 outline-1 h-9 outline-white rounded-lg w-full pt-2 pb-1 mb-3`}
+                            placeholder={lang["cards_card_number_placeholder"]}
+                          />
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div className={type!=="Card"?"flex flex-col":"hidden"}>
-                  <span
-                    className={`text-${oppositeTheme} text-xl font-${font}-bold mt-1`}
-                  >
-                    {lang["cards_shaba_number"]}
-                  </span>
-                  <div className=" w-full">
-                    <div className="w-full flex mt-0 px-2">
-                      <input
-                        onChange={(e) => setShabaNumber(e.target.value)}
-                        className={`flex-1 hide-input-arrows text-center-important font-${font}-regular text-${oppositeTheme} border border-gray bg-${theme} px-3 outline-1 h-9 outline-white rounded-lg w-full pt-2 pb-1 mb-3`}
-                        placeholder={lang["cards_shaba_number_placeholder"]}
-                      />
+                    <div className={type!=="Card"?"flex flex-col":"hidden"}>
+                      <span
+                        className={`text-${oppositeTheme} text-xl font-${font}-bold mt-1`}
+                      >
+                        {lang["cards_shaba_number"]}
+                      </span>
+                      <div className=" w-full">
+                        <div className="w-full flex mt-0 px-2">
+                          <input
+                            onChange={(e) => setShabaNumber(e.target.value)}
+                            className={`flex-1 hide-input-arrows text-center-important font-${font}-regular text-${oppositeTheme} border border-gray bg-${theme} px-3 outline-1 h-9 outline-white rounded-lg w-full pt-2 pb-1 mb-3`}
+                            placeholder={lang["cards_shaba_number_placeholder"]}
+                          />
+                        </div>
+                      </div>
                     </div>
-                  </div>
+                    <div
+                      className="flex justify-end"
+                    >
+                      <button className={type.length!==0?"bg-blue-gradient rounded-xl text-white w-1/4 h-1/3 pt-1 mt-5":"hidden"}>
+                        {lang["submit"]}
+                      </button>
+                    </div>
                 </div>
-                <div
-                  style={{ gridRow: 5, gridColumn: 1 }}
-                  className="flex justify-end"
-                >
-                  <button className="bg-blue-gradient rounded-xl text-white w-1/4 h-1/3 pt-1 mt-5">
-                    {lang["submit"]}
-                  </button>
-                </div>
-            </div>
           </form>
         </div>
       </div>
---
+++ src/pages/Submitting/Exchange/Pagination.jsx
@@ -0,0 +1,80 @@
+import "../pagination.css";
+import React, { useState } from "react";
+import { useThemeState } from "../../../Providers/ThemeProvider";
+import ReactPaginate from "react-paginate";
+import Rows from "./rows";
+
+const Pagination = ({ exchanges }) => {
+  const theme = useThemeState();
+  const oppositeTheme = theme === "dark" ? "blue" : "blue";
+  const paginationActive = `
+  .paginationActive a {
+    color: "aqua";
+    background-color: "aliceblue";
+}`;
+
+  const [pageNumber, setPageNumber] = useState(0);
+  const usersPerPage = 11;
+  const pagesVisited = pageNumber * usersPerPage;
+  console.log(exchanges);
+  const displayUsers = exchanges
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((data, index) => {
+      return (
+        <>
+          <div>
+            {index % 9 === 0 ? (
+              <Rows index={index} exchanges={data} />
+            ) : index % 9 === 1 ? (
+              <Rows index={index} exchanges={data} />
+            ) : index % 9 === 2 ? (
+              <Rows index={index} exchanges={data} />
+            ) : index % 9 === 3 ? (
+              <Rows index={index} exchanges={data} />
+            ) : index % 9 === 4 ? (
+              <Rows index={index} exchanges={data} />
+            ) : index % 9 === 5 ? (
+              <Rows index={index} exchanges={data} />
+            ) : index % 9 === 6 ? (
+              <Rows index={index} exchanges={data} />
+            ) : index % 9 === 7 ? (
+              <Rows index={index} exchanges={data} />
+            ) : (
+              <Rows index={index} exchanges={data} />
+            )}
+          </div>
+        </>
+      );
+    });
+
+  const pageCount = Math.ceil(exchanges.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <>
+      {displayUsers}
+      <div className="Page">
+        <ReactPaginate
+          previousLabel={"<"}
+          nextLabel={">"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={
+            theme === "dark" ? "paginationBttnsDark" : "paginationBttnsLight"
+          }
+          previousLinkClassName={"previousBttn"}
+          nextLinkClassName={"nextBttn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={
+            theme === "dark" ? "paginationActiveDark" : "paginationActiveLight"
+          }
+        />
+      </div>
+    </>
+  );
+};
+
+export default Pagination;
