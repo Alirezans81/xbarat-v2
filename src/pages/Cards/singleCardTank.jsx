354a8dabcbc0b2c4f4c544472c6cb3a0cdae04c8
Sina <sinabook20031381@gmail.com>
2024-01-01 16:26:01 +0330

    Edit cards 50%

--- src/pages/Cards/singleCardTank.jsx
+++ src/pages/Cards/singleCardTank.jsx
@@ -4,26 +4,48 @@ import { useLanguageState } from "../../Providers/LanguageProvider";
 import starChecked from "../../Images/pages/layout/Profile/starChecked.png";
 import starUnChecked from "../../Images/pages/layout/Profile/starUnChecked.png";
 import edit from "../../Images/pages/layout/Profile/editBlue.png"
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import { useEditWalletTanks } from "../../apis/common/wallet/hooks";
+import { useIsLoadingSplashScreenSetState } from "../../Providers/IsLoadingSplashScreenProvider";
+import EditCards from "./editcard";
 const SingleCardTank = ({ show, index, data }) => {
   const theme = useThemeState();
   const oppositeTheme = theme === "dark" ? "light" : "dark";
-  const wallet = useWalletState();
   const lang = useLanguageState();
-  const walletTanks = wallet.walletTanks.filter(
-    (data) => data.currency_abb === show
-  );
-  const [isChecked, setIsChecked] = useState(walletTanks.is_favorite);
+  const setIsLoadingSplashScreen = useIsLoadingSplashScreenSetState();
+
+  const { editWalletTank, isLoading: editWalletTankIsLoading } =
+      useEditWalletTanks();
+  useEffect(() => {
+    setIsLoadingSplashScreen(editWalletTankIsLoading);
+  }, [editWalletTankIsLoading]);
+
+  const [title,setTitle]=useState(data.title)  
+  const [bankInfo,setBankInfo]=useState(data.bank_info);
+  const [walletTankType,setWalletTankType]=useState(data.wallet_tank_type);
+  const [user,setUser]=useState(data.user);
+  const [isChecked, setIsChecked] = useState(data.is_favorite);
+  const [isActive,setIsActive]=useState(!data.is_deleted);
+  const [editCards,setEditCards]=useState(false)
 
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
   };
 
   function handleIsActive() {
-    console.log("s");
+    setIsActive(!isActive)
   }
+  const handleEditCard=()=>{
+    setEditCards(true);
+    console.log(editCards)
+
+  }
+
+  
     return (
     <>
+      <EditCards editCards={editCards} setEditCards={setEditCards} data={data}/>
+
       {/* This is for lg screen */}
       <div
         className={`xs:hidden lg:block bg-${theme}-back w-11/12 rounded-3xl ml-5 mt-5`}
@@ -34,13 +56,13 @@ const SingleCardTank = ({ show, index, data }) => {
       >
         <div className="w-full h-full flex flex-col p-6 px-9">
           <div className="flex flex-row h-1/4 w-full">
-            <span className="text-blue text-3xl w-5/6 h-full flex justify-start min-w-0 ">{data.title}</span>
-            <button className="flex justify-end w-1/12 h-full mt-1"><img alt="" src={edit} style={{width:"62%",height:"62%"}}/></button>
-            <button className="flex justify-end w-1/12 h-full"><img alt="" src={starChecked} style={{width:"80%",height:"80%"}}/></button>
+            <span className="text-blue text-3xl w-5/6 h-full flex justify-start min-w-0 ">{title}</span>
+            <button onClick={handleEditCard}className="flex justify-end w-1/12 h-full mt-1"><img alt="" src={edit} style={{width:"62%",height:"62%"}}/></button>
+            <button onClick={handleCheckboxChange} className="flex justify-end w-1/12 h-full"><img alt="" src={isChecked?starChecked:starUnChecked} style={{width:"80%",height:"80%"}}/></button>
           </div>
           <div className="w-full h-1/2 flex justify-start flex-col" style={{marginTop:"5%"}}>
                   <span className="text-gray text-2xl w-full h-1/2 min-w-0 ">{lang["bank_name_null_cards"]}</span>
-                  <span className={`text-${oppositeTheme} text-2xl min-w-0 `} >{data.bank_info}</span>
+                  <span className={`text-${oppositeTheme} text-2xl min-w-0 `} >{bankInfo}</span>
           </div>
           <div className="flex justify-end w-full h-1/4 items-end">
             <div className="w-full flex justify-end">
@@ -48,6 +70,7 @@ const SingleCardTank = ({ show, index, data }) => {
                     <label className={`text-${oppositeTheme} text-2xl`}>Is Active</label>
                     <input type="checkbox"
                      onChange={handleIsActive}
+                     value={isActive}
                      className="bg-transparent border-2 border-solid border-blue rounded-sm focus:border-0 ml-1 mb-3"/>
               </form>
             </div>
@@ -65,13 +88,13 @@ const SingleCardTank = ({ show, index, data }) => {
       > 
         <div className="w-full h-full flex flex-col p-6 px-9">
           <div className="flex flex-row h-1/4 w-full">
-            <span className="text-blue text-3xl w-5/6 h-full flex justify-start min-w-0 ">{data.title}</span>
-            <button className="flex justify-end w-1/12 h-full mt-1"><img alt="" src={edit} style={{width:"62%",height:"62%"}}/></button>
-            <button className="flex justify-end w-1/12 h-full"><img alt="" src={starChecked} style={{width:"80%",height:"80%"}}/></button>
+            <span className="text-blue text-3xl w-5/6 h-full flex justify-start min-w-0 ">{title}</span>
+            <button onClick={handleEditCard} className="flex justify-end w-1/12 h-full mt-1"><img alt="" src={edit} style={{width:"62%",height:"62%"}}/></button>
+            <button onClick={handleCheckboxChange} className="flex justify-end w-1/12 h-full"><img alt="" src={isChecked?starChecked:starUnChecked} style={{width:"80%",height:"80%"}}/></button>
           </div>
           <div className="w-full h-1/2 flex justify-start flex-col" style={{marginTop:"5%"}}>
                   <span className="text-gray text-lg w-full h-1/2 min-w-0">{lang["bank_name_null_cards"]}</span>
-                  <span className={`text-${oppositeTheme} text-xl min-w-0 `} >{data.bank_info}</span>
+                  <span className={`text-${oppositeTheme} text-xl min-w-0 `} >{bankInfo}</span>
           </div>
           <div className="flex justify-end w-full h-1/4 items-end">
             <div className="w-full flex justify-end">
@@ -79,6 +102,8 @@ const SingleCardTank = ({ show, index, data }) => {
                     <label className={`text-${oppositeTheme} text-2xl`}>Is Active</label>
                     <input type="checkbox"
                      onChange={handleIsActive}
+                     value={isActive}
+
                      className="bg-transparent border-2 border-solid border-blue rounded-sm focus:border-0 ml-1 mb-3"/>
               </form>
             </div>
@@ -95,13 +120,13 @@ const SingleCardTank = ({ show, index, data }) => {
       > 
         <div className="w-full h-full flex flex-col p-6 px-9">
           <div className="flex flex-row h-1/4 w-full">
-            <span className="text-blue text-3xl w-5/6 h-full flex justify-start min-w-0 ">{data.title}</span>
-            <button className="flex justify-end w-1/12 h-full mt-1"><img alt="" src={edit} style={{width:"62%",height:"62%"}}/></button>
-            <button className="flex justify-end w-1/12 h-full"><img alt="" src={starChecked} style={{width:"80%",height:"80%"}}/></button>
+            <span className="text-blue text-3xl w-5/6 h-full flex justify-start min-w-0 ">{title}</span>
+            <button onClick={handleEditCard} className="flex justify-end w-1/12 h-full mt-1"><img alt="" src={edit} style={{width:"62%",height:"62%"}}/></button>
+            <button onClick={handleCheckboxChange} className="flex justify-end w-1/12 h-full"><img alt="" src={isChecked?starChecked:starUnChecked} style={{width:"80%",height:"80%"}}/></button>
           </div>
           <div className="w-full h-1/2 flex justify-start flex-col" style={{marginTop:"5%"}}>
                   <span className="text-gray text-lg w-full h-1/2 min-w-0">{lang["bank_name_null_cards"]}</span>
-                  <span className={`text-${oppositeTheme} text-xl min-w-0 `} >{data.bank_info}</span>
+                  <span className={`text-${oppositeTheme} text-xl min-w-0 `} >{bankInfo}</span>
           </div>
           <div className="flex justify-end w-full h-1/4 items-end">
             <div className="w-full flex justify-end">
@@ -109,6 +134,7 @@ const SingleCardTank = ({ show, index, data }) => {
                     <label className={`text-${oppositeTheme} text-2xl`}>Is Active</label>
                     <input type="checkbox"
                      onChange={handleIsActive}
+                     value={isActive}
                      className="bg-transparent border-2 border-solid border-blue rounded-sm focus:border-0 ml-1 mb-3"/>
               </form>
             </div>
