29b10054721f289fd0cb40ccafe7ba331bdf3e56
Alirezans81 <99117010+Alirezans81@users.noreply.github.com>
2024-02-07 16:30:03 +0330

    add discription wallet tank in pending deposite request - making step 4 not nesessary

--- src/components/modals/CompleteProfileModal/Step4.js
+++ src/components/modals/CompleteProfileModal/Step4.js
@@ -57,188 +57,209 @@ export default function Step4({
 
   if (handleBlur && handleChange && values) {
     return (
-      <div className="w-full flex gap-x-10 my-5">
-        <div className="flex-1">
-          <form className="w-full h-full">
-            <div className="flex-1 w-full flex flex-col gap-y-2">
-              <span className={`font-${font}-regular text-${oppositeTheme}`}>
-                {lang["currency"]}
-              </span>
-              <div className="w-full flex">
-                <CustomDropdown
-                  label={
-                    selectedCurrencyIndex >= 0
-                      ? currencies[selectedCurrencyIndex].abbreviation
-                      : ""
-                  }
-                >
-                  {currencies.map((currency, index) => {
-                    if (index === 0 && index === currencies.length - 1) {
-                      return (
-                        <CustomItem
-                          key={index}
-                          className="rounded-xl"
-                          onClick={() => setSelectedCurrencyIndex(index)}
-                        >
-                          {currency && currency.abbreviation
-                            ? currency.abbreviation
-                            : "error"}
-                        </CustomItem>
-                      );
-                    } else if (index === 0) {
-                      return (
-                        <CustomItem
-                          key={index}
-                          className="rounded-t-xl"
-                          onClick={() => setSelectedCurrencyIndex(index)}
-                        >
-                          {currency && currency.abbreviation
-                            ? currency.abbreviation
-                            : "error"}
-                        </CustomItem>
-                      );
-                    } else if (index === currencies.length - 1) {
-                      return (
-                        <CustomItem
-                          key={index}
-                          className="rounded-b-xl"
-                          onClick={() => setSelectedCurrencyIndex(index)}
-                        >
-                          {currency && currency.abbreviation
-                            ? currency.abbreviation
-                            : "error"}
-                        </CustomItem>
-                      );
-                    } else {
-                      return (
-                        <CustomItem
-                          key={index}
-                          onClick={() => setSelectedCurrencyIndex(index)}
-                        >
-                          {currency && currency.abbreviation
-                            ? currency.abbreviation
-                            : "error"}
-                        </CustomItem>
-                      );
-                    }
-                  })}
-                </CustomDropdown>
-              </div>
-            </div>
-            <div className="flex-1 w-full flex flex-col gap-y-2 mt-5">
-              <span className={`font-${font}-regular text-${oppositeTheme}`}>
-                {lang["type-of-document"]}
-              </span>
-              <div className="w-full flex">
-                <CustomDropdown
-                  label={
-                    selectedWalletTankType >= 0
-                      ? walletTankTypes[selectedWalletTankType].title
-                      : ""
-                  }
-                >
-                  {walletTankTypes.map((walletTankType, index) => {
-                    if (index === 0 && index === walletTankTypes.length - 1) {
-                      return (
-                        <CustomItem
-                          key={index}
-                          className="rounded-xl"
-                          onClick={() => setSelectedWalletTankType(index)}
-                        >
-                          {walletTankType && walletTankType.title
-                            ? walletTankType.title
-                            : "error"}
-                        </CustomItem>
-                      );
-                    } else if (index === 0) {
-                      return (
-                        <CustomItem
-                          key={index}
-                          className="rounded-t-xl"
-                          onClick={() => setSelectedWalletTankType(index)}
-                        >
-                          {walletTankType && walletTankType.title
-                            ? walletTankType.title
-                            : "error"}
-                        </CustomItem>
-                      );
-                    } else if (index === walletTankTypes.length - 1) {
-                      return (
-                        <CustomItem
-                          key={index}
-                          className="rounded-b-xl"
-                          onClick={() => setSelectedWalletTankType(index)}
-                        >
-                          {walletTankType && walletTankType.title
-                            ? walletTankType.title
-                            : "error"}
-                        </CustomItem>
-                      );
-                    } else {
-                      return (
-                        <CustomItem
-                          key={index}
-                          onClick={() => setSelectedWalletTankType(index)}
-                        >
-                          {walletTankType && walletTankType.title
-                            ? walletTankType.title
-                            : "error"}
-                        </CustomItem>
-                      );
-                    }
-                  })}
-                </CustomDropdown>
-              </div>
-            </div>
-            <div className="flex-1 w-full flex flex-col gap-y-2 mt-5">
-              <span className={`font-${font}-regular text-${oppositeTheme}`}>
-                {lang["bank-account-title"]}
-              </span>
-              <div className="w-full flex">
-                <input
-                  className={`flex-1 hide-input-arrows bg-${theme}-back font-${font}-regular text-${oppositeTheme} px-3 outline-1 h-9 outline-white rounded-lg w-0 pt-2 pb-1`}
-                  name="title"
-                  onBlur={handleBlur("title")}
-                  onChange={handleChange("title")}
-                  value={values.title ? values.title : ""}
-                />
-              </div>
-            </div>
-            <div className="flex-1 w-full flex flex-col gap-y-2 mt-5">
-              <span className={`font-${font}-regular text-${oppositeTheme}`}>
-                {lang["bank-account-number"]}
-              </span>
-              <div className="w-full flex">
-                <input
-                  className={`flex-1 hide-input-arrows bg-${theme}-back font-${font}-regular text-${oppositeTheme} px-3 outline-1 h-9 outline-white rounded-lg w-0 pt-2 pb-1`}
-                  name="bank_info"
-                  onBlur={handleBlur("bank_info")}
-                  onChange={handleChange("bank_info")}
-                  value={values.bank_info ? values.bank_info : ""}
-                />
-              </div>
-            </div>
-          </form>
-        </div>
+      <div className="flex flex-col">
         <div
-          className={`flex-1 hidden md:flex flex-col py-5 px-7 bg-${theme}-glass rounded-2xl`}
+          className={`w-full bg-${theme}-glass px-0.5 py-3 md:py-1 rounded-xl flex items-center`}
         >
-          <div className="flex items-center gap-x-2">
-            <img
-              className="w-6 h-6"
-              src={require(`../../../Images/common/info-${oppositeTheme}.png`)}
-              alt="info"
-            />
-            <span className={`font-${font}-bold text-${oppositeTheme} pt-1.5`}>
-              {lang["note"]}
-            </span>
+          <img
+            className="w-10 h-10"
+            src={require(`../../../Images/exclamation.png`)}
+            alt="info"
+          />
+          <span
+            className={`text-yellow text-sm md:text-base font-${font}-regular -mb-1`}
+          >
+            {lang["complete-profile-step-4-not-required-message-1st"] +
+              ". " +
+              lang["complete-profile-step-4-not-required-message-2nd"] +
+              "."}
+          </span>
+        </div>
+        <div className="w-full flex gap-x-10 my-5">
+          <div className="flex-1">
+            <form className="w-full h-full">
+              <div className="flex-1 w-full flex flex-col gap-y-2">
+                <span className={`font-${font}-regular text-${oppositeTheme}`}>
+                  {lang["currency"]}
+                </span>
+                <div className="w-full flex">
+                  <CustomDropdown
+                    label={
+                      selectedCurrencyIndex >= 0
+                        ? currencies[selectedCurrencyIndex].abbreviation
+                        : ""
+                    }
+                  >
+                    {currencies.map((currency, index) => {
+                      if (index === 0 && index === currencies.length - 1) {
+                        return (
+                          <CustomItem
+                            key={index}
+                            className="rounded-xl"
+                            onClick={() => setSelectedCurrencyIndex(index)}
+                          >
+                            {currency && currency.abbreviation
+                              ? currency.abbreviation
+                              : "error"}
+                          </CustomItem>
+                        );
+                      } else if (index === 0) {
+                        return (
+                          <CustomItem
+                            key={index}
+                            className="rounded-t-xl"
+                            onClick={() => setSelectedCurrencyIndex(index)}
+                          >
+                            {currency && currency.abbreviation
+                              ? currency.abbreviation
+                              : "error"}
+                          </CustomItem>
+                        );
+                      } else if (index === currencies.length - 1) {
+                        return (
+                          <CustomItem
+                            key={index}
+                            className="rounded-b-xl"
+                            onClick={() => setSelectedCurrencyIndex(index)}
+                          >
+                            {currency && currency.abbreviation
+                              ? currency.abbreviation
+                              : "error"}
+                          </CustomItem>
+                        );
+                      } else {
+                        return (
+                          <CustomItem
+                            key={index}
+                            onClick={() => setSelectedCurrencyIndex(index)}
+                          >
+                            {currency && currency.abbreviation
+                              ? currency.abbreviation
+                              : "error"}
+                          </CustomItem>
+                        );
+                      }
+                    })}
+                  </CustomDropdown>
+                </div>
+              </div>
+              <div className="flex-1 w-full flex flex-col gap-y-2 mt-5">
+                <span className={`font-${font}-regular text-${oppositeTheme}`}>
+                  {lang["type-of-document"]}
+                </span>
+                <div className="w-full flex">
+                  <CustomDropdown
+                    label={
+                      selectedWalletTankType >= 0
+                        ? walletTankTypes[selectedWalletTankType].title
+                        : ""
+                    }
+                  >
+                    {walletTankTypes.map((walletTankType, index) => {
+                      if (index === 0 && index === walletTankTypes.length - 1) {
+                        return (
+                          <CustomItem
+                            key={index}
+                            className="rounded-xl"
+                            onClick={() => setSelectedWalletTankType(index)}
+                          >
+                            {walletTankType && walletTankType.title
+                              ? walletTankType.title
+                              : "error"}
+                          </CustomItem>
+                        );
+                      } else if (index === 0) {
+                        return (
+                          <CustomItem
+                            key={index}
+                            className="rounded-t-xl"
+                            onClick={() => setSelectedWalletTankType(index)}
+                          >
+                            {walletTankType && walletTankType.title
+                              ? walletTankType.title
+                              : "error"}
+                          </CustomItem>
+                        );
+                      } else if (index === walletTankTypes.length - 1) {
+                        return (
+                          <CustomItem
+                            key={index}
+                            className="rounded-b-xl"
+                            onClick={() => setSelectedWalletTankType(index)}
+                          >
+                            {walletTankType && walletTankType.title
+                              ? walletTankType.title
+                              : "error"}
+                          </CustomItem>
+                        );
+                      } else {
+                        return (
+                          <CustomItem
+                            key={index}
+                            onClick={() => setSelectedWalletTankType(index)}
+                          >
+                            {walletTankType && walletTankType.title
+                              ? walletTankType.title
+                              : "error"}
+                          </CustomItem>
+                        );
+                      }
+                    })}
+                  </CustomDropdown>
+                </div>
+              </div>
+              <div className="flex-1 w-full flex flex-col gap-y-2 mt-5">
+                <span className={`font-${font}-regular text-${oppositeTheme}`}>
+                  {lang["bank-account-title"]}
+                </span>
+                <div className="w-full flex">
+                  <input
+                    className={`flex-1 hide-input-arrows bg-${theme}-back font-${font}-regular text-${oppositeTheme} px-3 outline-1 h-9 outline-white rounded-lg w-0 pt-2 pb-1`}
+                    name="title"
+                    onBlur={handleBlur("title")}
+                    onChange={handleChange("title")}
+                    value={values.title ? values.title : ""}
+                  />
+                </div>
+              </div>
+              <div className="flex-1 w-full flex flex-col gap-y-2 mt-5">
+                <span className={`font-${font}-regular text-${oppositeTheme}`}>
+                  {lang["bank-account-number"]}
+                </span>
+                <div className="w-full flex">
+                  <input
+                    className={`flex-1 hide-input-arrows bg-${theme}-back font-${font}-regular text-${oppositeTheme} px-3 outline-1 h-9 outline-white rounded-lg w-0 pt-2 pb-1`}
+                    name="bank_info"
+                    onBlur={handleBlur("bank_info")}
+                    onChange={handleChange("bank_info")}
+                    value={values.bank_info ? values.bank_info : ""}
+                  />
+                </div>
+              </div>
+            </form>
           </div>
           <div
-            className={`mt-4 flex flex-col gap-y-3 text-gray font-${font}-regular w-64`}
+            className={`flex-1 hidden md:flex flex-col py-5 px-7 bg-${theme}-glass rounded-2xl`}
           >
-            <span>{lang["complete-profile-modal-step4-note-1st"] + "."}</span>
-            <span>{lang["complete-profile-modal-step4-note-2nd"] + "."}</span>
-            <span>{lang["complete-profile-modal-step4-note-3rd"] + "."}</span>
+            <div className="flex items-center gap-x-2">
+              <img
+                className="w-6 h-6"
+                src={require(`../../../Images/common/info-${oppositeTheme}.png`)}
+                alt="info"
+              />
+              <span
+                className={`font-${font}-bold text-${oppositeTheme} pt-1.5`}
+              >
+                {lang["note"]}
+              </span>
+            </div>
+            <div
+              className={`mt-4 flex flex-col gap-y-3 text-gray font-${font}-regular w-64`}
+            >
+              <span>{lang["complete-profile-modal-step4-note-1st"] + "."}</span>
+              <span>{lang["complete-profile-modal-step4-note-2nd"] + "."}</span>
+              <span>{lang["complete-profile-modal-step4-note-3rd"] + "."}</span>
+            </div>
           </div>
         </div>
       </div>
