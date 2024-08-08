591ab2b3486a6282c331e929dc11db024d98835e
nikiabed <nikiabedzade@gmail.com>
2024-08-08 11:05:58 +0330

    Referral Event/exchange/register & EditReferral component

--- src/pages/Profile/Referral.js
+++ src/pages/Profile/Referral.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useThemeState } from "../../Providers/ThemeProvider";
 import { useFontState } from "../../Providers/FontProvider";
 import { useLanguageState } from "../../Providers/LanguageProvider";
+import EditReferral from "../../components/pages/layout/Referral/EditReferral";
 
 export default function Referral() {
   const theme = useThemeState();
@@ -12,34 +13,30 @@ export default function Referral() {
   return (
     <>
       <div
-        className={`grid grid-cols-12 grid-row-2 md:gap-x-10 gap-y-7 w-full h-full text-${oppositeTheme} font-${font}-regular `}
+        className={`grid grid-cols-12 grid-row-2 md:gap-x-10 gap-y-7  text-${oppositeTheme} font-${font}-regular overflow-y-auto min-h-0 max-h-full w-full`}
       >
         <div
-          className={` rounded-2xl col-start-1 col-end-4 py-5 pl-5 bg-${theme} `}
+          className={` rounded-2xl col-start-1 col-end-4 py-5 px-6 bg-${theme} `}
         >
-          <h1>Referral Code</h1>
+          <span>Referral Code</span>
         </div>
 
         <div
           className={` rounded-2xl col-start-4 col-end-8 py-5 px-6 bg-${theme} `}
         >
-          <h3>Receive</h3>
+          <span>Receive</span>
         </div>
 
         <div
-          className={`rounded-2xl col-start-8 col-end-13 row-start-1 row-end-3 w-full py-5 px-6 bg-${theme} font-${font}-regular`}
+          className={` col-start-8 col-end-13 row-start-1 row-end-3 bg-${theme} font-${font}-regular   `}
         >
-          <button>
-            <h3>Events</h3>
-          </button>
-          <h3>Exchanges</h3>
-          <h3>Registered</h3>
+          <EditReferral />
         </div>
 
         <div
           className={` rounded-2xl col-start-1 col-end-8 row-start-2 row-end-2 py-5 px-6 bg-${theme} font-${font}-regular`}
         >
-          <h2>person code</h2>
+          <span>person code</span>
         </div>
       </div>
     </>
