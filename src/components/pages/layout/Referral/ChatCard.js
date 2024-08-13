3134a0be1b1986d9f073b02d00dc71733dba0571
nikiabed <nikiabedzade@gmail.com>
2024-08-13 10:09:32 +0330

    Chat card + chats status bar

--- src/components/pages/layout/Referral/ChatCard.js
+++ src/components/pages/layout/Referral/ChatCard.js
@@ -2,19 +2,24 @@ import React from "react";
 
 export default function ChatCard() {
   return (
-    <div className="flex flex-col text-light bg-dark-back h-44 w-[79] rounded-2xl px-3 py-3 justify-between">
+    <div className="flex flex-col text-light bg-dark-back max-h-44 max-w-72 rounded-2xl px-3 py-3 justify-between">
       <div>
         <div className="flex justify-between text-xl ">
           <span>Cancel Request</span>
           <img
             src={require("../../../../Images/pages/Tickets/Ticket sign.png")}
-            className="w-7 h-7"
+            className="w-7 h-5"
           />
         </div>
         <span className="text-blue text-xs">New Message</span>
-        <div>
-          <span className="text-xs text-gray">
-            You should check your wallet first and then try again
+        <div className="line-clamp-2 w-64">
+          <span className=" text-xs text-gray ">
+            You should check your wallet first and then try again Fugiat aute
+            dolor tempor amet elit consequat fugiat eu. Non elit veniam in qui
+            ullamco. Exercitation esse ut velit magna et velit duis laborum.
+            Nulla veniam qui voluptate officia quis. Cillum id pariatur sint
+            nostrud incididunt eiusmod cillum voluptate aliqua. Officia anim
+            veniam qui cupidatat dolor Lorem in culpa consectetur eu.
           </span>
         </div>
       </div>
--- src/components/pages/layout/Tickets/Chats.js
+++ src/components/pages/layout/Tickets/Chats.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useFontState } from "../../../../Providers/FontProvider";
 import { useThemeState } from "../../../../Providers/ThemeProvider";
 import ChatCard from "../Referral/ChatCard";
+import CustomDateTimeInput from "../../../common/CustomDateTimePicker";
+import { CustomDropdown, CustomItem } from "../../../common/CustomDropdown";
 export default function Chats({ topic }) {
   const theme = useThemeState();
   const oppositeTheme = theme === "dark" ? "light" : "dark";
@@ -19,9 +21,34 @@ export default function Chats({ topic }) {
     );
   } else {
     return (
-      <div className="w-full h-full flex flex-wrap gap-x-5 px-5 py-5">
-        <ChatCard />
-      </div>
+      <>
+        <div className=" w-full h-full overflow-y-scroll">
+          <div className="flex content-center justify-between px-12 pt-5 ">
+            <div>
+              <div className="flex gap-x-3">
+                <CustomDropdown label={"Status"} />
+                <CustomDateTimeInput placeHolder={"From Date"} />
+                <CustomDateTimeInput placeHolder={"From Date"} />
+              </div>
+            </div>
+            <div>
+              <img
+                src={require("../../../../Images/pages/Tickets/Ticket sign.png")}
+                className="w-6 h-6"
+              />
+            </div>
+          </div>
+          <div className="w-full h-full flex flex-wrap px-12 py-5 gap-6 ">
+            <ChatCard />
+            <ChatCard />
+            <ChatCard />
+            <ChatCard />
+            <ChatCard />
+            <ChatCard />
+            <ChatCard />
+          </div>
+        </div>
+      </>
     );
   }
 }
